45805fdf9946b0001ac68e68c80c79da2cf1fdbc
TijnDN <tijnden@gmail.com>
2025-03-19 08:57:39 +0100

    code gevoegd
    code gevoegd

---
+++ Tower-spel/Assets/playerInvetory.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInvetory : MonoBehaviour
+{
+    [Header("General")]
+
+    public List<itemType> inventoryList;
+    public int selectedItem;
+
+    [Space(20)]
+    [Header("Keys")]
+    [SerializeField] KeyCode throwItemKey;
+    [SerializeField] KeyCode pickItemKey;
+
+    [Space(20)]
+    [Header("Item gameobjects")]
+
+    void Update()
+    {
+
+        
+    }
+}
