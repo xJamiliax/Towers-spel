8a4b4bd2ec5282a32854a1e1f3a27ea3338d8917
TijnDN <tijnden@gmail.com>
2025-03-28 10:27:12 +0100

    code en assets gevoegd

---
+++ Tower-spel/Assets/movement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Unity.Properties;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Vector3 moveDir;
+    public float moveSpeed;
+    public Vector3 bodyRotate;
+    public float rotateSpeed;
+    public Transform cam;
+    public Vector3 camRotate;
+    public float camRotateSpeed;
+    float xRotation;
+    float yRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        moveDir.x = Input.GetAxis("Horizontal");
+        moveDir.z = Input.GetAxis("Vertical");
+        transform.Translate(moveDir * Time.deltaTime * moveSpeed);
+
+
+
+        bodyRotate.y = Input.GetAxis("Mouse X");
+        transform.Rotate(bodyRotate * Time.deltaTime * camRotateSpeed);
+
+
+        camRotate.x = Input.GetAxis("Mouse Y");
+        cam.Rotate(-camRotate * Time.deltaTime * camRotateSpeed);
+
+
+
+    }
+}
